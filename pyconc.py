6f20413f8940499b2be25f3c2c50af150835f6d5
John Evans <john@milo.com>
2010-03-06 22:22:34 -0800

    Checking in broken multithreaded stuff so I can work on it remotely.

--- pyconc.py
+++ pyconc.py
@@ -1,3 +1,6 @@
+import threading
+
+
 def identity(x):
     return x
 
@@ -10,6 +13,9 @@ class Empty(object):
     def to_list(self):
         return []
 
+    def __repr__(self):
+        return "Empty()"
+
 
 class Singleton(object):
 
@@ -21,6 +27,9 @@ class Singleton(object):
 
     def to_list(self):
         return [self.value]
+
+    def __repr__(self):
+        return "Singleton(%s)" % repr(self.value)
 
 
 class Concat(object):
@@ -38,16 +47,28 @@ class Concat(object):
     def to_list(self):
         return self.left.to_list() + self.right.to_list()
 
+    def __repr__(self):
+        return "Concat(%s, %s)" % (repr(self.left), repr(self.right))
+
 
 class SingleThreadedMultiplexor(object):
 
     def map_reduce(self, clist, mapf, reducef, initial=None):
         if isinstance(clist, Empty):
-            return self._map_reduce_empty(clist, initial)
+            print "using empty map reducer"
+            result = self._map_reduce_empty(clist, initial)
+            print "returning result from empty: %s" % str(result)
+            return result
         if isinstance(clist, Singleton):
-            return self._map_reduce_singleton(clist, mapf, reducef, initial)
+            print "using singleton map reducer"
+            result = self._map_reduce_singleton(clist, mapf, reducef, initial)
+            print "returning result from singleton: %s" % str(result)
+            return result
         if isinstance(clist, Concat):
-            return self._map_reduce_concat(clist, mapf, reducef, initial)
+            print "using concat map reducer"
+            result = self._map_reduce_concat(clist, mapf, reducef, initial)
+            print "returning result from concat: %s" % str(result)
+            return result
         raise TypeError("Unexpected clist type: %s" % clist.__class__.__name__)
 
     def _map_reduce_empty(self, clist, initial):
@@ -71,6 +92,63 @@ class SingleThreadedMultiplexor(object):
         if clist.left and clist.right:
             result = reducef(sub_map_reduce(clist.left, x_initial=initial),
                              sub_map_reduce(clist.right))
+        elif clist.left:
+            result = sub_map_reduce(clist.left, x_initial=initial)
+        elif clist.right:
+            result = sub_map_reduce(clist.right, x_initial=initial)
+        return result
+
+
+class MassiveMultiThreadedMultiplexor(SingleThreadedMultiplexor):
+    """You would pretty much never want to use this in production, but
+    here we go for a quick test.
+
+    """
+
+    @staticmethod
+    def _check_and_reduce_results(results, reducef):
+        if "left_exception" in results:
+            print "Left exception: %s" % results["left_exception"]
+        if "right_exception" in results:
+            print "Right exception: %s" % results["right_exception"]
+        print "results: %s" % repr(results)
+        result = reducef(results["left"], results["right"])
+        print "Reduced result: %s" % str(result)
+        return result
+
+    def _map_reduce_concat(self, clist, mapf, reducef, initial):
+
+        def sub_map_reduce(target, x_initial=None):
+            return self.map_reduce(target, mapf, reducef, initial=x_initial)
+
+        if clist.left and clist.right:
+            results = {}
+
+            def do_side(results, name, item, x_initial):
+                print "do_side...item: %s" % repr(item)
+                try:
+                    results[name] = sub_map_reduce(item, x_initial=x_initial)
+                except Exception, ex:
+                    results[name + "_exception"] = ex
+                print "returning results: %s" % repr(results)
+
+            left_t = threading.Thread(target=do_side, args=(results,
+                                                            "left",
+                                                            clist.left,
+                                                            initial))
+            right_t = threading.Thread(target=do_side, args=(results,
+                                                             "right",
+                                                             clist.right,
+                                                             None))
+            threads = (left_t, right_t)
+            for t in threads:
+                t.start()
+            for t in threads:
+                if t.isAlive():
+                    print "Waiting for t..."
+                    t.join()
+
+            return self._check_and_reduce_results(results, reducef)
         elif clist.left:
             result = sub_map_reduce(clist.left, x_initial=initial)
         elif clist.right:
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
 import operator
+import time
 
+from datetime import datetime
 from copy import copy
 
 from pyconc import Empty
@@ -9,6 +11,11 @@ from pyconc import Concat
 from pyconc import identity
 
 from pyconc import SingleThreadedMultiplexor
+from pyconc import MassiveMultiThreadedMultiplexor
+
+
+def square(n):
+    return n * n
 
 
 class ConcTests(unittest.TestCase):
@@ -23,6 +30,7 @@ class ConcTests(unittest.TestCase):
         self.one_234 = Concat(Concat(Singleton(1), Singleton(2)),
                               Concat(Singleton(3), Singleton(4)))
         self.st_multiplexor = SingleThreadedMultiplexor()
+        self.mt_multiplexor = MassiveMultiThreadedMultiplexor()
 
     def test_identity(self):
         for thing in [None, 5, "foo", object(), Empty(), [1, 2, 3],
@@ -52,38 +60,123 @@ class ConcTests(unittest.TestCase):
         self.assertEquals([42, 42, 42, 42], self.two_by_two.to_list())
         self.assertEquals([1, 2, 3, 4], self.one_234.to_list())
 
-    def test_singlethreaded_mapreduce_empty_no_initial_value(self):
+    def test_singlethreaded_reduce_empty_no_initial_value(self):
         f = lambda: self.st_multiplexor.map_reduce(self.empty,
                                                    None, operator.__add__)
         self.assertRaises(TypeError, f)
 
-    def test_singlethreaded_mapreduce_empty_with_initial_value(self):
+    def test_singlethreaded_reduce_empty_with_initial_value(self):
         self.assertEquals(10, self.st_multiplexor.map_reduce(self.empty,
                                                              None,
                                                              operator.__add__,
                                                              initial=10))
 
-    def test_singlethreaded_mapreduce_singleton__no_initial_value(self):
+    def test_singlethreaded_reduce_singleton__no_initial_value(self):
         self.assertEquals(42, self.st_multiplexor.map_reduce(self.single,
                                                              None,
                                                              operator.__add__))
 
-    def test_singlethreaded_mapreduce_singleton_with_initial_value(self):
+    def test_singlethreaded_reduce_singleton_with_initial_value(self):
         self.assertEquals(52, self.st_multiplexor.map_reduce(self.single,
                                                              None,
                                                              operator.__add__,
                                                              initial=10))
 
-    def test_singlethreaded_mapreduce_concat_no_initial_value(self):
+    def test_singlethreaded_reduce_concat_no_initial_value(self):
         self.assertEquals(10, self.st_multiplexor.map_reduce(self.one_234,
                                                              None,
                                                              operator.__add__))
 
-    def test_singlethreaded_mapreduce_concat_with_initial_value(self):
+    def test_singlethreaded_reduce_concat_with_initial_value(self):
         self.assertEquals(20, self.st_multiplexor.map_reduce(self.one_234,
                                                              None,
                                                              operator.__add__,
                                                              initial=10))
+
+    def test_singlethreaded_mapreduce_empty_no_initial_value(self):
+        f = lambda: self.st_multiplexor.map_reduce(self.empty,
+                                                   square,
+                                                   operator.__add__)
+        self.assertRaises(TypeError, f)
+
+    def test_singlethreaded_mapreduce_empty_with_initial_value(self):
+        self.assertEquals(10, self.st_multiplexor.map_reduce(self.empty,
+                                                             square,
+                                                             operator.__add__,
+                                                             initial=10))
+
+    def test_singlethreaded_mapreduce_singleton__no_initial_value(self):
+        self.assertEquals(1764, self.st_multiplexor.map_reduce(self.single,
+                                                             square,
+                                                             operator.__add__))
+
+    def test_singlethreaded_mapreduce_singleton_with_initial_value(self):
+        self.assertEquals(1774, self.st_multiplexor.map_reduce(self.single,
+                                                             square,
+                                                             operator.__add__,
+                                                             initial=10))
+
+    def test_singlethreaded_mapreduce_concat_no_initial_value(self):
+        self.assertEquals(30, self.st_multiplexor.map_reduce(self.one_234,
+                                                             square,
+                                                             operator.__add__))
+
+    def test_singlethreaded_mapreduce_concat_with_initial_value(self):
+        self.assertEquals(40, self.st_multiplexor.map_reduce(self.one_234,
+                                                             square,
+                                                             operator.__add__,
+                                                             initial=10))
+
+    def test_multithreaded_reduce_empty_no_initial_value(self):
+        f = lambda: self.mt_multiplexor.map_reduce(self.empty,
+                                                   None, operator.__add__)
+        self.assertRaises(TypeError, f)
+
+    def test_multithreaded_reduce_empty_with_initial_value(self):
+        self.assertEquals(10, self.mt_multiplexor.map_reduce(self.empty,
+                                                             None,
+                                                             operator.__add__,
+                                                             initial=10))
+
+    def test_multithreaded_reduce_singleton__no_initial_value(self):
+        self.assertEquals(42, self.mt_multiplexor.map_reduce(self.single,
+                                                             None,
+                                                             operator.__add__))
+
+    def test_multithreaded_mapreduce_singleton_with_initial_value(self):
+        self.assertEquals(52, self.mt_multiplexor.map_reduce(self.single,
+                                                             None,
+                                                             operator.__add__,
+                                                             initial=10))
+
+    def test_multithreaded_reduce_concat_no_initial_value(self):
+        self.assertEquals(10, self.mt_multiplexor.map_reduce(self.one_234,
+                                                             None,
+                                                             operator.__add__))
+
+    def test_multithreaded_reduce_concat_with_initial_value(self):
+        self.assertEquals(20, self.mt_multiplexor.map_reduce(self.one_234,
+                                                             None,
+                                                             operator.__add__,
+                                                             initial=10))
+
+    def test_multithreaded_slow_map_is_parallelized(self):
+        # This is a bit misleading due to GIL issues, but just for
+        # testing in theory....
+
+        def slow_identity(n):
+            time.sleep(50)
+            return n
+
+        start = datetime.now()
+        result = self.mt_multiplexor.map_reduce(self.one_234,
+                                                slow_identity,
+                                                operator.__add__,
+                                                initial=10)
+        diff = datetime.now() - start
+
+        self.assert_(diff.microseconds < 10000)
+        self.assertEquals(20, result)
 
 
 if __name__ == '__main__':
